1ba6551bbf13485ceb0800a4a88daac931312c5c
Emmanuel Obiajulu Okoye <>
2021-04-06 19:38:36 +0000

    Initial commit

---
+++ RockPaperScissor.py
@@ -0,0 +1,103 @@
+import random
+
+#asking the user for an input
+users_Choice = int(input ('''What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissor.\n''' ))
+
+# using conditional to decide where Rock Paper and Scissor should be placed
+if users_Choice == 0:
+    print('''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+    ''')
+elif users_Choice== 1:
+    print('''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+    ''')
+elif users_Choice== 2:
+    print('''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+    ''')
+
+    
+#computer Part of the game    
+print ("Computer Choose:")
+
+#computer choosing at random
+computerChoice = int(random.choice(["0", "1", "2"]))
+
+if computerChoice == 0:
+    print('''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+    ''')
+elif computerChoice == 1:
+    print('''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+    ''')
+elif computerChoice == 2:
+    print('''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+    ''')
+
+                #GAME CONDITION
+# Draw Condition
+if users_Choice == computerChoice:
+    print ("DRAW")
+    
+#Rock wins against scissors condition
+elif users_Choice == 0 and computerChoice == 2:
+    print ("⚡⚡⚡ YOU WIN!!! ⚡⚡⚡")
+    print ("✨✨✨ CONGRATULATION ✨✨✨")
+elif users_Choice == 2 and computerChoice == 0:
+    print ("🛡🛡🛡 COMPUTER WIN 🛡🛡🛡")
+    print ("PLEASE TRY AGAIN")
+    
+#Scissors win against paper condition
+elif users_Choice == 2 and computerChoice == 1:
+    print ("⚡⚡⚡ YOU WIN!!! ⚡⚡⚡")
+    print ("✨✨✨ CONGRATULATION ✨✨✨")
+elif users_Choice == 1 and computerChoice == 2:
+    print ("🛡🛡🛡 COMPUTER WIN 🛡🛡🛡")
+    print ("PLEASE TRY AGAIN")
+    
+#Paper wins against rock condition
+elif users_Choice == 1 and computerChoice == 0:
+    print ("⚡⚡⚡ YOU WIN!!! ⚡⚡⚡")
+    print ("✨✨✨ CONGRATULATION ✨✨✨")
+elif users_Choice == 0 and computerChoice == 1:
+    print ("🛡🛡🛡 COMPUTER WIN 🛡🛡🛡")
+    print ("PLEASE TRY AGAIN")
+
+#expectional conditions
+elif users_Choice >= 3 or users_Choice < 0:
+     print ("You Inserted the Wrong Input")
+     print("Please Input a Number in range of 0 to 2")
+
